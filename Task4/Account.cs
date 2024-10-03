ba3575010735a275a0c700b71ab483e567830bbe
darwish74 <mohammeddarwish66609@gmail.com>
2024-10-03 15:04:56 +0300

    Add project files.

---
+++ Task4/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Account
+    {
+        public Account(string name, object name1, double balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
+
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public bool Deposit(double value)
+        {
+            if (value < 0)
+            {
+                return false;
+            }
+            else
+            {
+                Balance += value;
+                return true;    
+            }
+        }
+        public bool Withdraw(double value)
+        {
+            if (Balance-value>=0)
+            {
+                Balance -= value;   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Task4/SavingAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class SavingAccount:Account
+    {
+        public SavingAccount(double interestRate):base(string name, double balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public double InterestRate { get; set; }
+    }
+}
