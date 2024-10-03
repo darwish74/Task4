7cc6b46051b3414f9353fdc973157c85419e50e8
darwish74 <mohammeddarwish66609@gmail.com>
2024-10-03 18:44:57 +0300

    to

--- Task4/TrustAccount.cs
+++ Task4/TrustAccount.cs
@@ -8,11 +8,32 @@ namespace Task4
 {
     internal class TrustAccount:SavingAccount 
     {
-        public double Fees { get; set; }
+        public int Withdrawablecount { get; set; }
         public TrustAccount(string name,double balance,double interestRate):
         base(name, balance, interestRate)
         {
-    
+         
+        }
+        public new bool Deposit(double amount)
+        {
+            if (amount >= 5000)
+            {
+                return base.Deposit(amount + 50);
+            }
+            else
+            {
+                return base.Deposit(amount);
+            }
+        }
+
+        public new bool Withdraw (double amount) 
+        {
+            if(Withdrawablecount<=3 && amount>=Balance*0.2)
+            {
+                Withdrawablecount++;
+                return base.Withdraw(amount);
+            }
+            return false;
         }
     }
 }
